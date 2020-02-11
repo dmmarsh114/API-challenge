f16e4931d7ef171cdbebfa063aee780fe61e5a29
dmmarsh114 <49419067+dmmarsh114@users.noreply.github.com>
2020-02-11 14:12:31 -0500

    finalizing functionality

--- DnD API/dndAPI.js
+++ DnD API/dndAPI.js
@@ -3,62 +3,103 @@ let baseURL = 'http://www.dnd5eapi.co'
 let mainList = document.getElementById('mainList');
 let selectedList = document.getElementById('selectedList');
 
-function fetchBase() {
-    fetch(baseURL + '/api/')
+function getSpells() {
+    fetch(baseURL + '/api/spells')
     .then(response => response.json())
-    .then(jsonData => displayBase(jsonData))
+    .then(jsonData => displaySpells(jsonData))
 }
 
-fetchBase();
+getSpells();
 
-function displayBase(data) {
+function displaySpells(data) {
+    let results = data.results;
+    results.forEach(item => {
+        let spell = document.createElement('li');
+        mainList.appendChild(spell);
 
-    console.log(data);
+        let infoButton = document.createElement('button');
+        infoButton.innerText = item.name;
+        spell.appendChild(infoButton);
 
-    for (let item in data) {
-
-        let itemEndpoint = data[item];
-
-        let listItem = document.createElement('li');
-        
-        let itemButton = document.createElement('button');
-        itemButton.innerText = `Get me ${item}`;
-        itemButton.addEventListener('click', function(e, endpoint, name) {
-            endpoint = itemEndpoint;
-            name = item;
-            getStuff(endpoint, name);
-        })
-
-        mainList.appendChild(listItem);
-        listItem.appendChild(itemButton);
-    }
-}
-
-function getStuff(endpoint, name) {
-    fetch(baseURL + endpoint)
-    .then(response => response.json())
-    .then(jsonData => {
-        console.log(`${name}:`, jsonData.results)
-        displayStuff(jsonData.results);
+        infoButton.addEventListener('click', function(e, url) {
+            url = item.url;
+            getMoreInfo(url);
+        });
     })
 }
 
-// function displayStuff(data) {
-//     for (let item in data) {
+function getMoreInfo(url) {
+    fetch(baseURL + url)
+    .then(response => response.json())
+    .then(jsonData => {
+        console.log(jsonData);
+        displayMoreInfo(jsonData);
+    })    
+}
 
-//         let itemEndpoint = data[item];
+function displayMoreInfo(data) {
 
-//         let listItem = document.createElement('li');
-        
-//         let itemButton = document.createElement('button');
-//         itemButton.innerText = `Get me ${item}`;
-//         itemButton.addEventListener('click', function(e, endpoint, name) {
-//             endpoint = itemEndpoint;
-//             name = item;
-//             getStuff(endpoint, name);
-//         })
+    while (selectedList.firstChild) {
+        selectedList.removeChild(selectedList.firstChild);
+    }
 
-//         selectedList.appendChild(listItem);
-//         listItem.appendChild(itemButton);
-//     }
-// }
+    let spellHeading = document.createElement('h3');
+    spellHeading.innerText = data.name;
+    
+    let school = document.createElement('p');
+    school.innerText = 'School of ' + data.school.name;
+
+    let classHeading = document.createElement('p');
+    classHeading.innerText = 'Classes:';
+
+    let classes = document.createElement('p');
+    classHeading.appendChild(classes);
+
+    data.classes.forEach(i => {
+        let x = document.createElement('li');
+        x.innerText = i.name;
+        classes.appendChild(x);
+    })
+
+    let infoList = document.createElement('ul');
+
+    let spellLevel = document.createElement('li');
+    spellLevel.innerText = 'Level: ' + data.level;
+    
+    let range = document.createElement('li');
+    range.innerText = 'Range: ' + data.range;
+    
+    let castingTime = document.createElement('li');
+    castingTime.innerText = 'Casting Time: ' + data.casting_time;
+    
+    let duration = document.createElement('li');
+    duration.innerText = 'Duration: ' + data.duration;
+    
+    let pageNum = document.createElement('li');
+    pageNum.innerText = 'Page Number: ' + data.page;
+
+    selectedList.appendChild(spellHeading);
+    selectedList.appendChild(school);
+    selectedList.appendChild(classHeading);
+    selectedList.appendChild(infoList);
+    infoList.appendChild(spellLevel);
+    infoList.appendChild(range);
+    infoList.appendChild(castingTime);
+    infoList.appendChild(duration);
+    infoList.appendChild(pageNum);
+    
+    let description = data.desc;
+    description.forEach(line => {
+        let para = document.createElement('p');
+        para.innerText = line;
+        selectedList.appendChild(para);
+    })    
+
+    if (data.higher_level) {
+        data.higher_level.forEach(line => {
+            let para2 = document.createElement('p');
+            para2.innerText = line;
+            selectedList.appendChild(para2);
+        })
+    }
+}
